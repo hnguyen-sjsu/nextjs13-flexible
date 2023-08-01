67bd224a95361d01c1452832ecda3d5ab23e6f26
Kenny Nguyen <hien.p.nguyen01@sjsu.edu>
2023-08-01 11:05:52 -0700

    Update project

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,9 +2,11 @@ import { NavLinks } from '@/constant'
 import Image from 'next/image'
 import Link from 'next/link'
 import AuthProviders from './AuthProviders'
+import ProfileMenu from './ProfileMenu'
+import { getCurrentUser } from '@/lib/session'
 
-const Navbar = () => {
-    const session = null
+const Navbar = async () => {
+    const session = await getCurrentUser()
 
     return (
         <nav className='flexBetween navbar'>
@@ -26,9 +28,9 @@ const Navbar = () => {
                 </ul>
             </div>
             <div className='flexCenter gap-4'>
-                {session ? (
+                {session?.user ? (
                     <>
-                        UserPhoto
+                        <ProfileMenu session={session} />
                         <Link href='/create-project'>Share Work</Link>
                     </>
                 ) : (
